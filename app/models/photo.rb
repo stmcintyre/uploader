acc5338075e94c0941d5e6213791472d4193a8d0
Steven McIntyre <steven.mcintyre@injixo.com>
2014-08-04 21:36:28 +0100

    Add photo validations
    Size, format & width

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,17 @@
 class Photo < ActiveRecord::Base
   dragonfly_accessor :image
+
+  validates :title, presence: true, length: {minimum: 2, maximum: 20}
+  validates :image, presence: true
+
+  validates :image, presence: true
+  validates_size_of :image, maximum: 500.kilobytes,
+                    message: "should be no more than 500 KB", if: :image_changed?
+
+  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false,
+                     message: "should be either .jpeg, .jpg, .png, .bmp", if: :image_changed?
+
+  validates_property :width, of: :image, in: (0..400),
+                           message: proc{ |actual, model| "Unlucky #{model.title} - was #{actual}" }
+
 end
