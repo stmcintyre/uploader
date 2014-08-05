3bc6d2f79ac1e73ebd86767dd46b8d7a080993f9
Steven McIntyre <steven.mcintyre@injixo.com>
2014-08-05 21:44:36 +0100

    Store and display image size

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803185509) do
+ActiveRecord::Schema.define(version: 20140805204235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140803185509) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "image_size"
   end
 
 end
