e2590b857efc7bd36e24a045ff53958c4282a2eb
Steven McIntyre <steven.mcintyre@injixo.com>
2014-10-18 18:07:54 +0100

    Store thumbnails rather than generate on-the-fly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805204235) do
+ActiveRecord::Schema.define(version: 20141018163501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20140805204235) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "image_size"
+  end
+
+  create_table "thumbs", force: true do |t|
+    t.string   "uid"
+    t.string   "job"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
